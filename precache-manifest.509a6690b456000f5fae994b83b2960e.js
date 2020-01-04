c1bd8cbc319374971acd854f74c50d3c1915a8fa
Marvin Orendain <marvinorendain@Marvins-MBP.home>
2020-01-04 01:31:34 -0500

    Updates

---
+++ precache-manifest.509a6690b456000f5fae994b83b2960e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27559929834a8febd41863679fa604d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7cae15bb05e54d072c22",
+    "url": "/static/css/main.2ac6c168.chunk.css"
+  },
+  {
+    "revision": "3bb39a612da10357f265",
+    "url": "/static/js/2.e6d339fc.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.e6d339fc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7cae15bb05e54d072c22",
+    "url": "/static/js/main.4b0818bd.chunk.js"
+  },
+  {
+    "revision": "48fcc0e3ccb1be1c0571",
+    "url": "/static/js/runtime-main.b2ec948b.js"
+  }
+]);
